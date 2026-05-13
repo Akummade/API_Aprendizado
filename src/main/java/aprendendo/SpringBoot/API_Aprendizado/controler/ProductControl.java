130e0c769952193984bc03fdf5cd99b8100c2175
Akuma <fe90pe2010@gmail.com>
2026-05-12 21:06:03 -0300

    Atualização com funcionalidade de Atualizar , pesquisar e deletar ... (CRUD primitivo)

--- src/main/java/aprendendo/SpringBoot/API_Aprendizado/controler/ProductControl.java
+++ src/main/java/aprendendo/SpringBoot/API_Aprendizado/controler/ProductControl.java
@@ -2,11 +2,11 @@ package aprendendo.SpringBoot.API_Aprendizado.controler;
 
 import aprendendo.SpringBoot.API_Aprendizado.model.Products;
 import aprendendo.SpringBoot.API_Aprendizado.repository.ProductsRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.annotations.Parameter;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController//essa classe vai receber requisições REST
@@ -20,12 +20,31 @@ public class ProductControl {
     }
 
     @PostMapping//Post --> Salvar dados , criar recurso sem exibição na URL
-    public Products SavePR(@RequestBody Products pr){//Produto -> recurso
-        System.out.println("Product Recive -> " + pr);
-        var id = UUID.randomUUID().toString();
-        pr.setID(id);
-        productsRepository.save(pr);
+    public List<Products> SavePR(@RequestBody List<Products> pr){//Produto -> recurso
+        for(Products i: pr){
+            System.out.println("Product Recive -> " + i);
+            var id = UUID.randomUUID().toString();
+            i.setID(id);
+            productsRepository.save(i);
+        }
         return pr;
     }
-
+    @GetMapping("/{id}")
+    public Products getProduct(@PathVariable("id") String id){
+        return productsRepository.findById(id).orElse(null);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") String id){
+        productsRepository.deleteById(id);
+    }
+    @PutMapping("/{id}")
+    public void refresh(@PathVariable("id") String id,
+                        @RequestBody Products pr){
+        pr.setID(id);
+        productsRepository.save(pr);
+    }
+    @GetMapping
+    public List<Products> searchPr(@RequestParam("name") String pr){
+        return productsRepository.findByname(pr);
+    }
 }
--- src/main/java/aprendendo/SpringBoot/API_Aprendizado/repository/ProductsRepository.java
+++ src/main/java/aprendendo/SpringBoot/API_Aprendizado/repository/ProductsRepository.java
@@ -3,4 +3,8 @@ package aprendendo.SpringBoot.API_Aprendizado.repository;
 import aprendendo.SpringBoot.API_Aprendizado.model.Products;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository<Products , String> { }
+import java.util.List;
+
+public interface ProductsRepository extends JpaRepository<Products, String> {
+    List<Products> findByname(String name);
+}
